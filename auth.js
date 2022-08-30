1c139966f21d027eb0293538b44e5bc03e09abc9
aabuelazm <abdot525@gmail.com>
2022-08-30 11:22:53 -0700

    Added user session middleware

--- auth.js
+++ auth.js
@@ -3,6 +3,7 @@ const express = require('express');
 const auth = express.Router()
 const bcrypt = require('bcryptjs');
 const pgp = require('pg-promise')();
+var session = require('express-session')
 const db = pgp({
   connectionString: process.env.DATABASE_URL// ,
   // ssl: { rejectUnauthorized: false }
@@ -10,10 +11,17 @@ const db = pgp({
 const saltRounds = 10;
 exports.auth = auth;
 exports.db = db;
+exports.session = session;
 
 // AUTH FUNCTIONS
 // Authentication Router
 // Handles HTTP requests that go to https://localhost:PORT/auth
+
+// Logout Handling
+auth.get('/logout', (req, res) => {
+  req.session.destroy((err) => { })
+  res.redirect('back')
+})
 
 // Login User function
 // Return Values:
@@ -33,14 +41,24 @@ async function loginUser(username, password) {
 }
 
 // Login page methods
-auth.get('/login', (req, res) => res.render('pages/auth/login', { title: 'Login', message: null })
-)
+auth.get('/login', (req, res) => res.render('pages/auth/login', {
+  title: 'Login',
+  message: null
+}))
 auth.post('/login', async (req, res) => {
   await loginUser(req.body.username, req.body.password).then(async (user) => {
     if (user) {
-      res.render('pages/auth/login', { title: 'Login', message: `Successfully logged in as ${user.username}` })
+      req.session.username = user.username
+      res.locals.user = user.username
+      res.render('pages/auth/login', {
+        title: 'Login',
+        message: `Successfully logged in as ${user.username}`
+      })
     } else {
-      res.render('pages/auth/login', { title: 'Login', message: "The username and password provided do not match our records." })
+      res.render('pages/auth/login', {
+        title: 'Login',
+        message: "The username and password provided do not match our records."
+      })
     }
   })
 })
@@ -60,11 +78,20 @@ async function registerUser(username, password) {
 }
 
 // Register page methods
-auth.get('/register', (req, res) => res.render('pages/auth/register', { title: 'Register', message: null }))
+auth.get('/register', (req, res) => res.render('pages/auth/register', {
+  title: 'Register',
+  message: null
+}))
 auth.post('/register', async (req, res) => {
   if (await registerUser(req.body.username, req.body.password)) {
-    res.render('pages/auth/register', { title: 'Register', message: `User "${req.body.username}" has been created.`})
+    res.render('pages/auth/register', {
+      title: 'Register',
+      message: `User "${req.body.username}" has been created.`
+    })
   } else {
-    res.render('pages/auth/register', { title: 'Register', message: `User "${req.body.username}" already exists.`})
+    res.render('pages/auth/register', {
+      title: 'Register',
+      message: `User "${req.body.username}" already exists.`
+    })
   }
 });
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express()
-const authjs = require('./auth.js');
-const auth = authjs.auth;
+const { auth, db, session } = require('./auth.js')
 const path = require('path');
-const db = authjs.db;
 const PORT = process.env.PORT || 5000
+
 
 // DATABASE CONFIG
 db.query("CREATE TABLE IF NOT EXISTS users ( \
@@ -18,15 +17,25 @@ db.query("CREATE TABLE IF NOT EXISTS users ( \
 // DEVELOPERS CODE ENDS HERE
 app.use(express.static(path.join(__dirname, 'public')))
   .use(express.urlencoded())
+  .use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 3600000 }
+  }))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .use((req, res, next) => {
+    res.locals.user = req.session.username
+    next()
+  })
+  .use('/auth', auth)
   // ROUTING EXAMPLES
   .get('/', (req, res) => res.render('pages/index', { title: 'Home' }))
-  .get('/help', (req, res) => res.render('pages/help', { title: 'Help' }))
+  .get('/help', (req, res) => res.render('pages/help', { title: 'Help'}))
   // ROUTING STARTS HERE
 
 
 
   // ROUTING ENDS HERE
-  .use('/auth', auth)
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
